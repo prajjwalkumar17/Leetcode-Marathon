32eaaf68fbb106e2c45409579c2772250ce3824c
prajjwalkumar17 <write2prajjwal@gmail.com>
2023-10-23 23:41:49 +0530

    init

---
+++ src/Arrays/ArrayInit.java
@@ -0,0 +1,12 @@
+package Arrays;
+public class ArrayInit {
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        int[] b = {1,2,3,4,5,5};
+        char[] alpha = new char[5];
+
+        for (int i=0; i<a.length; i++){
+            a[i] = 3;
+        }
+    }
+}
